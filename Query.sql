054abc5f36019d147760b4a1f95701c604bde0bf
CH0C0M1LK <127130183+CH0C0M1LK@users.noreply.github.com>
2023-05-21 00:18:53 -0400

    Update Query.sql

--- Query.sql
+++ Query.sql
@@ -1,8 +1,14 @@
-select player.user_name as player_name, roles.role_name as roles, team.team_name as team_name, matches.date_name, game_category.category as category, champion.champion as champion, score.kills || '/' || score.deaths || '/' || score.assists as KDA from game_match
+select player.user_name as player_name, roles.role_name as roles, team.team_name as team_name, matches.date_name, game_category.category as category, champion.champion as champion_selected, dragon_element.elemental || ' ' || dragon as dragons_killed, kills || '/' || deaths || '/' || assists as KDA
+from game_match
 join player on game_match.user_id = player.id
 join roles on game_match.role_id = roles.id
 join team on game_match.team_id = team.id
 join matches on game_match.match_id = matches.id
 join game_category on game_match.category_id = game_category.id
 join champion on game_match.champion_id = champion.id
-join score on game_match.score_id = score.id
+join dragon_element on game_match.element_id = dragon_element.id
+;
+
+select team.team_name, dragon_element.elemental || ' ' || dragon as dragons_killed from game_match
+join dragon_element on game_match.element_id = dragon_element.id
+join team on game_match.team_id = team.id
